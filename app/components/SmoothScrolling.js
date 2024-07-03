d6a9459449e20e62af97c447331b444c2b63dc5f
spyguy <shashankprasanna1@gmail.com>
2024-07-02 23:59:41 -0400

    attempt 3 to try to fix smooth scrolling

--- app/components/SmoothScrolling.js
+++ app/components/SmoothScrolling.js
@@ -3,7 +3,7 @@ import {ReactLenis} from "@studio-freight/react-lenis";
 
 function SmoothScrolling({children}) {
     return (
-        <ReactLenis root options={{lerp: 0.12}} syncTouch smoothWheel>
+        <ReactLenis root options={{lerp: 0.12, syncTouch: true, smoothWheel: true, infinite: true}}>
             {children}
         </ReactLenis>
     );
