ba9fc8c7a2f158e67f5d59a1abe9b380d43b3c4d
spyguy <shashankprasanna1@gmail.com>
2024-07-02 23:08:41 -0400

    try to fix smooth scrolling

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({children}) {
     return (
-        <html lang="en" className={'ff-no-scrollbar scroll-smooth'}>
+        <html lang="en" className={'ff-no-scrollbar'}>
             <body>
                 <SmoothScrolling>
                     {children}
