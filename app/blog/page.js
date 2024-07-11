513a2b2040274951a8cb204e4879bf6b0fbeffb5
spyguy <shashankprasanna1@gmail.com>
2024-07-11 19:47:54 -0400

    Fixed blog homepage & styled

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,45 +1,45 @@
 import {promises as fs} from 'fs';
-import {position} from "@chakra-ui/react";
+import Link from "next/link";
+import Footer from "@/app/components/mdx/Footer";
+
+async function getPosts() {
+    // Fetch all blog posts
+    const postFolderList = await fs.readdir('./app/(posts)'); // list of folders
+    let posts = [];
+    for (let i = 0; i < postFolderList.length; i++) {
+        let postMetadata = await fs.readFile(`./app/(posts)/${postFolderList[i]}/metadata.json`, 'utf-8');
+        postMetadata = JSON.parse(postMetadata);
+        posts.push(postMetadata);
+    }
+    console.log(posts);
+    return posts;
+}
 
 export default async function Page() {
-    async function getPosts() {
-        const posts = await fs.readdir(process.cwd() + '/app/(posts)')
-        let postData = []
-        posts.map(async post => {
-            const file = await fs.readFile(process.cwd() + '/app/(posts)/' + post + '/metadata.json', 'utf-8')
-            const data = JSON.parse(file);
-            postData.push(data)
-            console.log(postData.length)
-        })
-        console.log(postData)
-        return postData
-    }
-
-    async function mapPosts(){
-        return await getPosts()
-            .then(posts => {
-                return posts.map(post => {
-                    return (
-                        <div key={post.title}>
-                            <h1>{post.title}</h1>
-                            <p>{post.description}</p>
-                        </div>
-                    )
-                })
-
-            })
-    }
-
-    let data = await mapPosts()
+    const posts = await getPosts();
 
     return (
-        <div>
-            <h1>Blog Page</h1>
-            <ul>
-                <li>
-                    <p>{data}</p>
-                </li>
-            </ul>
+        <div className={'bg-slate-100 min-h-screen w-screen flex flex-col'}>
+            <h1 className={'font-inter font-semibold text-5xl pt-16 mx-auto'}>Blog</h1>
+            <div className={'grid grid-cols-2 mx-2 my-20'}>
+                {posts ? posts.map((post, index) => {
+                    return (
+                        <Link href={`/${post.ID}`} key={post.ID}>
+                            <div
+                                className={'border border-slate-400 hover:border-slate-600 hover:shadow-lg rounded-lg mx-4 p-4'}>
+                                <div className={'flex flex-col md:flex-row justify-between'}>
+                                    <h2 className={'font-inter font-semibold text-2xl mr-4'}>{post.title}</h2>
+                                    <p className={'mt-2 md:mt-0'}>{post.publishDate}</p>
+                                </div>
+                                <p className={'mt-2'}>{post.excerpt}</p>
+                            </div>
+                        </Link>
+                    )
+                }) : <p>Loading...</p>}
+            </div>
+            <div className={'w-fit h-fit mt-auto'}>
+                <Footer/>
+            </div>
         </div>
     )
 }
--- app/components/mdx/Footer.js
+++ app/components/mdx/Footer.js
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from "next/link";
 
 export default function Footer() {
     return (
@@ -9,11 +8,12 @@ export default function Footer() {
             </div>
             <div className={'w-1/2 h-full flex'}>
                 <div className={'mx-auto w-fit h-full flex flex-row'}>
-                    <a href={'https://www.linkedin.com/in/shashank-prasanna/'} className={'self-center mx-2'}>
+                    <a href={'https://www.linkedin.com/in/shashank-prasanna/'}
+                       className={'self-center mx-2 hover:scale-125'}>
                         <Image src={'/icons/linkedin.svg'} width={50} height={50}
                                alt={'linkedin'}/>
                     </a>
-                    <a href={'https://www.github.com/SpyGuy0215'} className={'self-center mx-2'}>
+                    <a href={'https://www.github.com/SpyGuy0215'} className={'self-center mx-2 hover:scale-125'}>
                         <Image src={'/icons/github.svg'} width={50} height={50}
                                alt={'linkedin'}/>
                     </a>
